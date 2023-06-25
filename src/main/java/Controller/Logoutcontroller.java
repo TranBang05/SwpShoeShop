483c00d5230e6238f1508ba8f8fe3bba8bbd0faa
bangtxhe18 <tranxuanbang.yt@gmail.com>
2023-06-25 20:46:26 +0700

    load db, update login, register

---
+++ src/main/java/Controller/Logoutcontroller.java
@@ -0,0 +1,18 @@
+package Controller;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "Logoutcontroller", value = "/Logoutcontroller")
+public class Logoutcontroller extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
